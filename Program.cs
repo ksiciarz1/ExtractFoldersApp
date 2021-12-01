f9d3e5fbcc4ba01e29d69437a26e3e9cfa42c5b1
ksiciarz <kacpersiciarz2001@gmail.com>
2021-12-01 14:46:00 +0100

    Added basic functionality.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ExtractFoldersApp
 {
@@ -6,7 +7,24 @@ namespace ExtractFoldersApp
     {
         static void Main(string[] args)
         {
+            var dirName = $@"./Excrated Folders";
+            string[] filePaths = Directory.GetFiles(@"./", "*.bmp", SearchOption.AllDirectories);
 
+            DirectoryInfo di = Directory.CreateDirectory(dirName);
+
+            try
+            {
+                System.Console.WriteLine($"Found {filePaths.Length} files. Do you want to continue ? (Y, N)");
+                string answer = System.Console.ReadLine();
+
+                foreach (string file in filePaths)
+                {
+                    Console.WriteLine($"Moving {file}...");
+                    File.Move(file, di.Name);
+                    Console.WriteLine($"Done!");
+                }
+            }
+            catch (IOException) { }
         }
     }
 }
